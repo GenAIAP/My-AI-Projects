306a606a1240952a6502900bb32918b13e79f3da
GenAIAP <bonbonpham245@gmail.com>
2026-09-20 21:34:55 +1000

    Update server.js

--- server.js
+++ server.js
@@ -91,5 +91,7 @@ const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0'
 server.listen(PORT, HOST, () => {
   console.log(`Socket.IO server running on port ${PORT}`);
-  initializePredictor();
+  setImmediate(() => {
+    initializePredictor();
+  });
 });
